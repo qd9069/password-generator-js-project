b94d140b974a8a4e3f65f4c0505789ac952ebaab
Elaine Deng <qd9069@gmail.com>
2022-03-31 02:16:44 -0700

    Added prompt,alert,and confirm windows

--- script.js
+++ script.js
@@ -1,9 +1,74 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// Declares variables for password length, lowercase, uppercase, numeric, special characters
+// var length - from prompt
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numeric = [0,1,2,3,4,5,6,7,8,9];
+var specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+
+var combineall = [];
+// lowercase.concat(uppercase,numeric,specialCharacters);
+// console.log (combineall);
+
+
+
+
+
 function generatePassword() {
   var password = "password";
   // TODO: add code to generate the password here
+
+  // 1- prompt for password length
+  var inputLength = window.prompt("How many characters would you like your password to contain?");
+
+  // 2- check if inputLength is valid, if not valid then stop. If valid continue.
+  if (inputLength < 8 || inputLength > 128) {
+    window.alert("The number of characters cannot be less than 8 and no more than 128, please enter it again.");
+  } else {
+    // 3- prompt question: to include special characters
+    var includeSpecialCharacters = window.confirm("Click OK to confirm including special characters.");
+    
+    // 4- prompt question: to include numeric characters
+    var includeNumeric = window.confirm("Click OK to confirm including numeric characters.");
+
+    // 5- prompt question: to include uppercase characters
+    var includeUppercase = window.confirm("Click OK to confirm including uppercase characters.");
+
+    // 6- prompt question: to include lowercase characters
+    var includeLowercase = window.confirm("Click OK to confirm including lowercase characters.");
+
+
+  }
+
+
+
+  // 7- generate password
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   return password;
 }
@@ -19,3 +84,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// 2) generate pw all "a"'s for the desired length
+// "aaaaaaa"
+// password = "";
+
+// for (var i=0; i < inputlength; i++) {
+// password = password + "a"
+
+// }
+
+// 3) display all random lowercase letters for the chose length
+
+// var lowercase = window.confirm("Click OK to confirm including lowercase characters.");
+
+// 4) display all random uppercase/lowercase letters
+// 5) let user choose to include uppercase and/or lowercase
+// 6) let user choose upper, lower, and/or numbers
+// 7) let user choose upper, lower, numbers, and/or specials
+// 8) prevent user from too short
+// 9) prevent user from too short or too long
+
+// 10) prevent user from choosing no character types
